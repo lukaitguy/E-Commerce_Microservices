6f91ce66366c80a3d37e4933fd5757bc3b531f2b
lukaitguy <ldjukic00@gmail.com>
2026-09-25 18:16:20 +0200

    Bug with assign roles fixed

--- E-Commerce.Services.AuthAPI/Services/AuthService.cs
+++ E-Commerce.Services.AuthAPI/Services/AuthService.cs
@@ -24,15 +24,15 @@ namespace E_Commerce.Services.AuthAPI.Services
         public async Task<bool> AssignRole(string email, string roleName)
         {
             var user = _db.ApplicationUsers.FirstOrDefault(u => u.Email.ToLower() == email.ToLower());
-            if(user != null)
+            if (user != null)
             {
-                if (!_roleManager.RoleExistsAsync(roleName).GetAwaiter().GetResult())
+                if (!await _roleManager.RoleExistsAsync(roleName))
                 {
-                    //create role
-                    _roleManager.CreateAsync(new IdentityRole(roleName)).GetAwaiter().GetResult();
-                    await _userManager.AddToRoleAsync(user, roleName);
-                    return true;
+                    await _roleManager.CreateAsync(new IdentityRole(roleName));
                 }
+
+                var result = await _userManager.AddToRoleAsync(user, roleName);
+                return result.Succeeded;
             }
             return false;
         }
